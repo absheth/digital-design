e25b0b97f9e56055899362c3a576c6e0e50854d1
Akash B. Sheth <absheth@iu.edu>
2018-09-17 12:13:07 -0400

    intermediate commit

--- labs/project_4/project_4.srcs/sim_1/new/top_tb.v
+++ labs/project_4/project_4.srcs/sim_1/new/top_tb.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 09/16/2018 09:41:28 PM
-// Design Name: 
+// Design Name:
 // Module Name: top_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -25,22 +25,29 @@ reg [15:0] sw;
 reg btnC;
 reg enable;
 wire [15:0] led;
+reg [7:0] Q, Qbar;
 
 top top_testbench(  .sw(sw[15:0]),
-                    .btnC(btnC),
-                    .enable(enable),
-                    .led(led[15:0])
-                    );
-
-initial begin
-    enable = 0;
-    
-    
-    
-end
+    .btnC(btnC),
+    .enable(enable),
+    .led(led[15:0])
+);
 
 always
-    #10
-    enable = !enable;
-    
+    #5 enable = !enable;
+
+initial begin
+
+    if(enable) begin
+
+
+    end
+
+
+end
+
+
+always
+    #7 Q[7:0] = sw[7:0];
+
 endmodule
